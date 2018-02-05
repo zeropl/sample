9dd3adddaeeb14310be2d42d67c163118dc5fa57
panney <zeropl@vip.qq.com>
2018-02-05 09:27:19 +0000

    xiawu

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,27 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Sample') - Laravel 入门教程</title>
-</head>
-<body>
-    @yield('content')
-</body>
+
+
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <div class="container">
+      @yield('content')
+    </div>    
+  </body>
 </html>
